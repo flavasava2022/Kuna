d86fe208723c47f8492e5a457a524b81867aeed9
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2023-09-20 21:57:38 +0300

    Add files via upload

--- javascript/table.js
+++ javascript/table.js
@@ -1,13 +1,132 @@
-// JavaScript
+var dataTable;
+
 $(document).ready(function () {
-  // Initialize DataTable with class selector
-  var tables = $(".dataTable").DataTable({
-    paging: true,
-    searching: true,
-    ordering: true,
+  // Initialize DataTable
+  loadDataTable();
+
+  // Add filtering
+  $("#searchInput").on("keyup", function () {
+    dataTable.search(this.value).draw();
+  });
+});
+
+function loadDataTable() {
+  dataTable = $("#dataTable1").DataTable({
+    ajax: {
+      url: "/Admin/Programs/getAll",
+      type: "GET",
+      dataType: "json",
+      dataSrc: "", // Data source is an empty string since we're providing data directly
+    },
+    columns: [
+      { data: "اسم الدوره" },
+      { data: "بدايه الدوره" },
+      { data: "نهايه الدوره" },
+      { data: "سعر الدوره" },
+      { data: "الاختيارات" },
+    ],
+    paging: true, // Enable pagination
+    pageLength: 5, // Set default page length to 5
     info: false,
     lengthChange: false,
-    pageLength: 5,
     pagingType: "numbers",
   });
+}
+
+// Mock server response (replace this with your actual server response)
+$.mockjax({
+  url: "/Admin/Programs/getAll",
+  responseText: [
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+      ><img src="imgs/eye.svg" alt=""
+    /></a>
+
+    <a href="Training-modification.html"
+      ><img src="imgs/edit.svg" alt=""
+    /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+    {
+      "اسم الدوره": "Program 1",
+      "بدايه الدوره": "2023-09-25",
+      "نهايه الدوره": "2023-09-30",
+      "سعر الدوره": "2023-09-30",
+      الاختيارات: `<a href="admin-courses.html"
+        ><img src="imgs/eye.svg" alt=""
+      /></a>
+  
+      <a href="Training-modification.html"
+        ><img src="imgs/edit.svg" alt=""
+      /></a>`,
+    },
+  ],
 });
